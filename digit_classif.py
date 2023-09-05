df9b7bca2071fbfa20c84a035b1a36df0f89073e
m22aie250@iitj.ac.in <root@Mustafa>
2023-09-05 18:36:33 +0530

    created separate util function

--- digit_classif.py
+++ digit_classif.py
@@ -1,68 +1,25 @@
-import matplotlib.pyplot as plt
-from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
+"""
+================================
+Recognizing hand-written digits
+================================
 
-def split_train_dev_test(X, y, test_size, dev_size):
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size, random_state=42)
-    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=dev_size, random_state=42)
-    return X_train, X_dev, X_test, y_train, y_dev, y_test
+This example shows how scikit-learn can be used to recognize images of
+hand-written digits, from 0-9.
 
-def predict_and_eval(model, X_test, y_test):
-    predicted = model.predict(X_test)
-    report = metrics.classification_report(y_test, predicted)
-    confusion_matrix = metrics.confusion_matrix(y_test, predicted)
-    return predicted, report, confusion_matrix
+"""
+
+##from util_fun import load_and_visualize_digits, preprocess_data, train_and_predict, visualize_predictions, evaluate_classifier
+from util_fun import load_and_visualize_digits, preprocess_data, train_classifier,predict_and_generate_report, visualize_predictions, evaluate_classifier
 
 
-# Load the digits dataset
-digits = datasets.load_digits()
+digits = load_and_visualize_digits()
+data = preprocess_data(digits)
+##X_test, predicted, y_test = train_and_predict(data, digits.target)
+classifier,X_train, X_test, y_train, y_test = train_classifier(data, digits.target)
+predicted, report = predict_and_generate_report(classifier, X_test, y_test)
+visualize_predictions(X_test, predicted)
+evaluate_classifier(y_test, predicted)
 
-# Flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
 
-# Split data into train, dev, and test subsets
-X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(data, digits.target, test_size=0.4, dev_size=0.2)
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
-
-# Predict and evaluate using the test set
-predicted, report, confusion_matrix = predict_and_eval(clf, X_test, y_test)
-print("Classification report for test set:\n", report)
-##print("Confusion matrix:\n", confusion_matrix)
-
-# Visualize the first 4 test samples and show their predicted digit value in the title
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-
-# Show the confusion matrix plot
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-plt.show()
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
